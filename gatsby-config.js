ee963ed96b2e8275267e7dc8fc6d419e51943d92
celineung <celine.ung@zenika.com>
2020-12-02 00:27:17 +0100

    style: add typography font theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
