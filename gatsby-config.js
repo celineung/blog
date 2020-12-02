372484246f1668a2c8ae5fed916398506d0a0b57
celineung <celine.ung@zenika.com>
2020-12-02 02:07:49 +0100

    refactor: move fav-icon image to assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -33,7 +33,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/src/assets`,
       },
     },
   ],
