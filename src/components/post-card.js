f68ca3415998b11ee9a54e6a408efa2174f52e38
celineung <celine.ung@zenika.com>
2020-12-08 23:51:15 +0100

    feat: add empty alt to descriptive post-card image

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -5,7 +5,7 @@ export default function PostCard({ title, date, category, excerpt, gatsyImage })
 
   return (
     <article className="post-card">
-      <Img fluid={ gatsyImage } />
+      <Img fluid={ gatsyImage } alt="" />
       <header className="post-card__header">
         <div className="post-card__category">{ category }</div>
         <h2 className="post-card__title">{ title }</h2>
