cc392f248f9c6479b4fa0913c8386450c1389931
celineung <celine.ung@zenika.com>
2020-12-14 23:06:02 +0100

    feat: add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     title: "Blog de Céline Ung",
     description: "Blog de Céline Ung",
     author: `@celineung`,
+    siteUrl: `https://celineung.github.io/blog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +12,13 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-fontawesome-css`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        exclude: [`/404`]
+      }
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
