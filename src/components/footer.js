73d6ffac733b2a26301e20426c7582eeed66076f
celineung <celine.ung@zenika.com>
2021-02-06 02:03:00 +0100

    fix: remove Link use for external links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -7,20 +6,20 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 export default function Footer() {
   return (
     <footer className="footer">
-      <Link
-        to="https://github.com/celineung/blog"
+      <a
+        href="https://github.com/celineung/blog"
         className="footer__link"
       >
         <FontAwesomeIcon icon={faGithub} aria-hidden="true"/>
         <span className="footer-link__text">Github</span>
-      </Link>
-      <Link
-        to="https://linkedin.com/in/celine-ung"
+      </a>
+      <a
+        href="https://linkedin.com/in/celine-ung"
         className="footer__link"
       >
         <FontAwesomeIcon icon={faLinkedin} aria-hidden="true"/>
         <span className="footer-link__text">LinkedIn</span>
-      </Link>
+      </a>
     </footer>
   )
 }
