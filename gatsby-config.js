86ee1692e9b5341ae5d7a68cc4f1e697277d1432
celineung <celine.ung@zenika.com>
2021-02-20 01:01:20 +0100

    feat: highlight code display

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,9 @@ module.exports = {
               maxWidth: 590,
             },
           },
+          {
+            resolve: `gatsby-remark-prismjs`,
+          },
         ],
       },
     },
