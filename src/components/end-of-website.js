5680b13cdfc20e49945af97cedfdb7e97c3c81e1
celineung <celine.ung@zenika.com>
2024-05-22 19:23:24 +0200

    add link to new website

---
+++ src/components/end-of-website.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {Link} from "gatsby";
+
+export default function EndOfWebsite() {
+  return (
+    <p class="end-of-site">
+      Ce blog sera prochainement désactivé car il a déménagé sur <a href="https://aycandoo.fr/blog/" target="_blank">AYCANDOO</a>.
+    </p>
+  )
+}
